b695d7a09fba5129b210e2d454bb68bfde044164
Edwin Ultengo <edwin.ultego@endava.com>
2022-05-18 07:52:35 -0500

    🐛 Fix Subscription & Backend

--- backend.tf
+++ backend.tf
@@ -1,8 +1,10 @@
 terraform {
   backend "azurerm" {
+    subscription_id      = "1afd90be-5a70-487b-83b4-88571e23e1ee"
     resource_group_name  = "eultengo-tfstate-rg"
-    storage_account_name = "eultengotfstaterampup"
+    storage_account_name = "eultengotfstateamweek"
     container_name       = "eultengotfstate"
     key                  = "global/infra/terraform.tfstate"
   }
 }
+
